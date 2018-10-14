eea2003f048c9204e5b4a3073afeffc4dc42ca90
root <1738127840@qq.com>
2018-10-14 19:54:46 +0800

    修改了一部分内容

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * @Auther:Cui LiHuan
+ * @Date: 2018/10/14 19:37
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        String[] ssplit = s.split("\\s+");
+        for (int i = 0; i < ssplit.length; i++) {
+            System.out.println(ssplit[i]);
+
+        }
+    }
+}
